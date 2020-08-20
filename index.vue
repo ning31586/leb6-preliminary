209d78fcbaea63956fcc7761ee790c0d066efaaa
Ning31586 <ning31586@hotmail.com>
2020-08-20 16:16:25 +0700

    Up

---
+++ index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>HP Player 100 </p> +รูป
+        <p>HP Monster 1050</p> +รูป
+        <p></p>
+
+        <button:click="up" class="btn bin-into">{{table}}</button>
+        <p v=bindistyle=""
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    <div>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+<script>
+
+
+
+
+
+
